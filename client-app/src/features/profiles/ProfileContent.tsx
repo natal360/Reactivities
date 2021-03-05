713141e213deef54ae81f4050ca4f5f0bdd4e957
natal <drossel.fb@gmail.com>
2021-03-05 19:21:50 +0900

    End of section 19

--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -1,28 +1,41 @@
-import { observer } from 'mobx-react-lite';
-import React from 'react'
-import { Tab } from 'semantic-ui-react'
-import { Profile } from '../../app/models/profile';
-import ProfilePhoto from './ProfilePhoto';
+import { observer } from "mobx-react-lite";
+import React from "react";
+import { Tab } from "semantic-ui-react";
+import { Profile } from "../../app/models/profile";
+import ProfileAbout from "./ProfileAbout";
+import ProfilePhoto from "./ProfilePhoto";
 
-interface Props{
+interface Props {
   profile: Profile;
 }
 
-export default observer(function ProfileContent({profile}: Props) {
+export default observer(function ProfileContent({ profile }: Props) {
   const panes = [
-    {menuItem: 'About', render: () => <Tab.Pane>About Content</Tab.Pane>},
-    {menuItem: 'Photos', render: () => <ProfilePhoto profile={profile} />},
-    {menuItem: 'Events', render: () => <Tab.Pane>Events Content</Tab.Pane>},
-    {menuItem: 'Followers', render: () => <Tab.Pane>Followers Content</Tab.Pane>},
-    {menuItem: 'Following', render: () => <Tab.Pane>Following Content</Tab.Pane>},
+    {
+      menuItem: "About",
+      render: () => (
+        <Tab.Pane>
+          <ProfileAbout />
+        </Tab.Pane>
+      ),
+    },
+    { menuItem: "Photos", render: () => <ProfilePhoto profile={profile} /> },
+    { menuItem: "Events", render: () => <Tab.Pane>Events Content</Tab.Pane> },
+    {
+      menuItem: "Followers",
+      render: () => <Tab.Pane>Followers Content</Tab.Pane>,
+    },
+    {
+      menuItem: "Following",
+      render: () => <Tab.Pane>Following Content</Tab.Pane>,
+    },
   ];
 
-  return(
-    <Tab 
-      menu={{fluid: true, vertical: true}}
-      menuPosition='right'
+  return (
+    <Tab
+      menu={{ fluid: true, vertical: true }}
+      menuPosition="right"
       panes={panes}
     />
-  )
-  
-})
+  );
+});
